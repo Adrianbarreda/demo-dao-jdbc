ec4cbd9258764bd664ff581df5466deffb26b772
Adrian Eduardo Gonzalez Barreda <81177394+Adrianbarreda@users.noreply.github.com>
2022-04-20 20:19:47 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,11 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createrSellerDao(); /*Posso acrecentar 
+		uma instanciação de um seller DAO. Dessa forma meu programa não conhece 
+		a implementação só conhece a interface. É também uma forma de fazer uma 
+		injeção de dependencia sem explicitar a implementação.*/
 		
 		System.out.println(seller);
 
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory { //"Fctory" é uma Classe auxiliar responsável por 
+						//instanciar os meus DAOs.	
+	
+	public static SellerDao createrSellerDao() {
+		return new SellerDaoJDBC();
+	}/* operação estática para instanciar os DAOs. O "createrSellerDao()" 
+	têm que retornar um SellerDao. Dessa forma a classe "DaoFactory" vai expor 
+	um método que retorna o tipo da interface --> "SellerDao"; 
+	mais internamente ela vai instanciar uma implementação
+	return new SellerDaoJDBC() é uma set para não necessitar expor a 
+	implementação, e deixar somente a interface*/ 
+	
+}
