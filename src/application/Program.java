0067ccc0d18ee698154a050278835a586552a72a
Adrian Eduardo Gonzalez Barreda <81177394+Adrianbarreda@users.noreply.github.com>
2022-05-25 15:02:34 -0400

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,35 @@
 package application;
 
+import java.util.List;
+
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+	
+		/*Scanner sc = new Scanner(System.in);*/
+		
 		SellerDao sellerDao = DaoFactory.createrSellerDao(); /*Posso acrecentar 
 		uma instanciação de um seller DAO. Dessa forma meu programa não conhece 
 		a implementação só conhece a interface. É também uma forma de fazer uma 
 		injeção de dependencia sem explicitar a implementação.*/
-		
-		System.out.println("=== TEST 1: seller findById ===");
-		Seller seller = sellerDao.findById(3);		
+				
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+			for (Seller obj : list) {
+				System.out.println(obj);
+			}		
 	}
 }
